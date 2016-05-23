7865167d07f3d7ba608f0c1095ec161c05c70516
odoepner <odoepner@gmail.com>
2016-05-23 14:27:19 -0300

    Fix project name

--- .travis/artifact-upload.sh
+++ .travis/artifact-upload.sh
@@ -4,7 +4,7 @@ user_info="$BINTRAY_USER:$BINTRAY_API_KEY"
 build_dir="$TRAVIS_BUILD_DIR/target"
 
 file="$(ls ${build_dir}/*.jar | head -n 1)"
-upload_url='https://api.bintray.com/content/odoepner/generic/baghchal/0.0.1/baghchal.jar?override=1&publish=1'
+upload_url='https://api.bintray.com/content/odoepner/generic/bagh-chal/0.0.1/bagh-chal.jar?override=1&publish=1'
 
 if [ -z "$file" ]; then
   echo "ERROR: Cannot upload. No jar file found in $build_dir"
