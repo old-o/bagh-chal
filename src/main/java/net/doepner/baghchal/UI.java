c9d0d52da4e27c49ee4899541ce45f0c44f68003
odoepner <oliver@doepner.net>
2016-06-16 14:57:44 -0300

    Organize drawing code into methods

--- src/main/java/net/doepner/baghchal/UI.java
+++ src/main/java/net/doepner/baghchal/UI.java
@@ -1,6 +1,12 @@
 package net.doepner.baghchal;
 
-import javax.swing.JPanel;
+import static java.awt.RenderingHints.KEY_ANTIALIASING;
+import static java.awt.RenderingHints.KEY_STROKE_CONTROL;
+import static java.awt.RenderingHints.KEY_TEXT_ANTIALIASING;
+import static java.awt.RenderingHints.VALUE_ANTIALIAS_ON;
+import static java.awt.RenderingHints.VALUE_STROKE_NORMALIZE;
+import static java.awt.RenderingHints.VALUE_TEXT_ANTIALIAS_ON;
+
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
@@ -15,12 +21,7 @@ import java.awt.image.BufferedImage;
 import java.util.HashMap;
 import java.util.Map;
 
-import static java.awt.RenderingHints.KEY_ANTIALIASING;
-import static java.awt.RenderingHints.KEY_STROKE_CONTROL;
-import static java.awt.RenderingHints.KEY_TEXT_ANTIALIASING;
-import static java.awt.RenderingHints.VALUE_ANTIALIAS_ON;
-import static java.awt.RenderingHints.VALUE_STROKE_NORMALIZE;
-import static java.awt.RenderingHints.VALUE_TEXT_ANTIALIAS_ON;
+import javax.swing.JPanel;
 
 public class UI extends JPanel {
 
@@ -78,7 +79,6 @@ public class UI extends JPanel {
         diagonalColor = getLevelColor("diagonalColor");
         gridColor = getLevelColor("gridColor");
         boardEdgeColor = getLevelColor("boardEdgeColor");
-
         repaint();
     }
 
@@ -99,10 +99,10 @@ public class UI extends JPanel {
         g2.setColor(getForeground());
 
         if (levels.isLevelDone()) {
-            g2.drawImage(congrats, 70, 80, null);
+            g2.drawImage(congrats, width / 2 - congrats.getWidth(null) / 2, height / 2- congrats.getHeight(null), null);
             g2.setFont(new Font("SansSerif", 0, 34));
             final String s = levels.getLevelEndMessage();
-            g2.drawString(s, width / 2 - (g2.getFontMetrics().stringWidth(s) >> 1), height / 2 + 100);
+            g2.drawString(s, width / 2 - (g2.getFontMetrics().stringWidth(s) / 2), height / 2 + 20);
         } else {
             drawBoard(g2, width, height);
             preyManager.drawDraggedPrey(g2);
@@ -110,12 +110,11 @@ public class UI extends JPanel {
     }
 
     void drawBoard(Graphics2D g2, int width, int height) {
+        setBackground(backgroundColor);
+        setOpaque(true);
 
         final int xStep = width / board.getXSize();
         final int yStep = height / board.getYSize();
-
-        setBackground(backgroundColor);
-        setOpaque(true);
 
         g2.translate(xStep / 2, yStep / 2);
 
@@ -125,22 +124,34 @@ public class UI extends JPanel {
         final int yStart = yStep;
         final int yEnd = yStep * board.getCentreYSize();
 
-        g2.setPaint(paint);
-        g2.fillRect(xStart - xStep /2, yStart - yStep/2, xEnd - xStart + xStep, yEnd - yStart + yStep);
+        drawBoardCentreArea(g2, xStep, yStep, xStart, xEnd, yStart, yEnd);
+        drawGridLines(g2, xStep, yStep, xStart, xEnd, yStart, yEnd);
+        drawDiagonalLines(g2, xStart, xEnd, yStart, yEnd);
+        drawPieces(g2, xStep, yStep);
 
-        g2.setStroke(stroke);
-        g2.setColor(boardEdgeColor);
-        g2.drawRect(xStart - xStep /2, yStart - yStep/2, xEnd - xStart + xStep, yEnd - yStart + yStep);
+        g2.translate(-xStep / 2, -yStep / 2);
+    }
 
+    private void drawGridLines(Graphics2D g2, int xStep, int yStep, int xStart, int xEnd, int yStart, int yEnd) {
         g2.setColor(gridColor);
-
         for (int x = xStart; x <= xEnd; x += xStep) {
             g2.drawLine(x, yStart, x, yEnd);
         }
         for (int y = yStart; y <= yEnd; y += yStep) {
             g2.drawLine(xStart, y, xEnd, y);
         }
+    }
 
+    private void drawBoardCentreArea(Graphics2D g2, int xStep, int yStep, int xStart, int xEnd, int yStart, int yEnd) {
+        g2.setPaint(paint);
+        g2.fillRect(xStart - xStep /2, yStart - yStep/2, xEnd - xStart + xStep, yEnd - yStart + yStep);
+
+        g2.setStroke(stroke);
+        g2.setColor(boardEdgeColor);
+        g2.drawRect(xStart - xStep /2, yStart - yStep/2, xEnd - xStart + xStep, yEnd - yStart + yStep);
+    }
+
+    private void drawDiagonalLines(Graphics2D g2, int xStart, int xEnd, int yStart, int yEnd) {
         final int xMid = (xStart + xEnd) / 2;
         final int yMid = (yStart + yEnd) / 2;
 
@@ -152,7 +163,9 @@ public class UI extends JPanel {
         g2.drawLine(xStart, yMid, xMid, yStart);
         g2.drawLine(xMid, yStart, xEnd, yMid);
         g2.drawLine(xMid, yEnd, xEnd, yMid);
+    }
 
+    private void drawPieces(Graphics2D g2, int xStep, int yStep) {
         for (int i = 0; i < board.getXSize(); i++) {
             for (int j = 0; j < board.getYSize(); j++) {
                 final Piece piece = board.get(i, j);
@@ -169,7 +182,5 @@ public class UI extends JPanel {
                 }
             }
         }
-
-        g2.translate(-xStep / 2, -yStep / 2);
     }
 }
