21783974e25057aef50b000945a4ff27d6c7a0cf
odoepner <oliver@doepner.net>
2016-12-23 13:01:46 -0400

    sometinhf

--- src/main/java/net/doepner/baghchal/Setup.java
+++ src/main/java/net/doepner/baghchal/Setup.java
@@ -1,8 +1,14 @@
 package net.doepner.baghchal;
 
+import net.doepner.baghchal.model.Direction;
 import net.doepner.baghchal.model.GameTable;
 import net.doepner.baghchal.model.Piece;
 import net.doepner.baghchal.model.Position;
+import net.doepner.baghchal.model.TablePositions;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 import static net.doepner.baghchal.model.Piece.PREDATOR;
 import static net.doepner.baghchal.model.Piece.PREY;
@@ -13,14 +19,47 @@ import static net.doepner.baghchal.model.Piece.PREY;
 public final class Setup {
 
     public void prepare(GameTable gameTable) {
-        setCornerPositions(gameTable, PREDATOR);
-        setBorderPositions(gameTable, PREY, gameTable.getPositions().getBoardSize() - 5);
+        final int boardSize = gameTable.getPositions().getBoardSize();
+        final int predatorCount = (boardSize / 5) - 1;
+        setCornerPositions(gameTable, PREDATOR, predatorCount);
+
+        setBorderPositions(gameTable, PREY, boardSize - 5);
     }
 
-    public void setCornerPositions(GameTable gameTable, Piece piece) {
+    public void setCornerPositions(GameTable gameTable, Piece piece, int predatorCount) {
+        int remaining = predatorCount;
+
         for (Position p : gameTable.getPositions().getCorners()) {
             gameTable.set(p, piece);
+            remaining--;
         }
+        for (Position p : getPositionsToFill(gameTable, remaining, piece)) {
+            gameTable.set(p, piece);
+        }
+    }
+
+    private Iterable<Position> getPositionsToFill(GameTable gameTable, int remaining, Piece piece) {
+        final TablePositions positions = gameTable.getPositions();
+        if (remaining > 0) {
+            final Collection<Position> toFill = new ArrayList<>();
+            final Iterable<Position> board = positions.getBoard();
+            for (Position p : board) {
+                if (gameTable.get(p) == piece) {
+                    for (Direction d : Direction.values()) {
+                        final Position p1 = d.addTo(p);
+                        if (positions.isBoard(p1) && gameTable.isEmptyAt(p1)) {
+                            toFill.add(p);
+                            remaining--;
+                            if (remaining <= 0) {
+                                return toFill;
+                            }
+                        }
+                    }
+                }
+            }
+            return toFill;
+        }
+        return Collections.emptyList();
     }
 
     public void setBorderPositions(GameTable gameTable, Piece piece, int count) {
