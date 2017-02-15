12ffd0b49a5dc6f183b8ba11580d2e53859f95da
odoepner <oliver@doepner.net>
2017-02-15 13:49:37 -0400

    Mark another FunctionalInterface as such

--- src/main/java/net/doepner/baghchal/model/GameTableFactory.java
+++ src/main/java/net/doepner/baghchal/model/GameTableFactory.java
@@ -3,6 +3,7 @@ package net.doepner.baghchal.model;
 /**
  * Creates GameTable instances
  */
+@FunctionalInterface
 public interface GameTableFactory {
 
     GameTable getGameTable(int xSize, int ySize);
